ccde7aa4c7b8231481d3325cfda84c761dda53ab
Mustafa  Developer <mustafadeveloper@macbook-pro.hexad.intra>
2018-06-28 11:17:20 +0200

    final revision

--- src/main/java/com/hexad/carRegistration/CarRegistrationApplication.java
+++ src/main/java/com/hexad/carRegistration/CarRegistrationApplication.java
@@ -1,6 +1,7 @@
 package com.hexad.carRegistration;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarRegistrationApplication {
